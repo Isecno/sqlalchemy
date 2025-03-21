03637d52b57e73fa3ec05dacce54a918af1915c1
George <isecrevezte@gmail.com>
2025-03-21 17:24:41 +0300

    Двойная защита

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
+
+app = Flask(__name__)
+
+
+@app.route('/<word>')
+@app.route('/index/<word>')
+def index(word):
+    return render_template('base.html', title=word)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, title="test")
+
+
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    return render_template('list_prof.html', param=list)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
